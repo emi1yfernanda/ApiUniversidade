5481367a6e3328804d16edac6aeeeefdb4ebd32a
emi1yfernanda <f.emily@escolar.ifrn.edu.br>
2023-09-25 10:25:41 -0300

    commit inicial

---
+++ Context/ApiuniversidadeContext.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Apiuniversidade.Model;
+
+namespace Apiuniversidade.Context
+{
+    public class ApiuniversidadeContext : DbContext
+    {
+        public ApiuniversidadeContext(DbContextOptions options) : base(options){}
+        // DbContextOptions é uma clase auxiliar para tranportar informações de configurações de acesso ao banco.
+        
+        public DbSet<Aluno>? Aluno {  get; set; }
+        public DbSet<Curso>? Curso {  get; set; }
+        public DbSet<Disciplina>? Disciplina {  get; set; }
+          
+    }
+}
